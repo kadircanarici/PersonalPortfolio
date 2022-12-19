c1a27f66dad79a5f1bdc1eb9c878f48873c5a57c
Kadircan Arıcı <kadircanarc@gmail.com>
2022-12-19 15:42:53 +0300

    into linesız olarak tamamlandı

--- PersonalPortfolio/wwwroot/js/Home/Intro.js
+++ PersonalPortfolio/wwwroot/js/Home/Intro.js
@@ -1,17 +1,14 @@
-﻿$(document).ready(function () {
-    WriteIntro();
+﻿
 
-}
-);
-function WriteIntro() {
     $.ajax('Home/GetIntros', {
         type: "GET",
         success: function (data) {
             $("#txtTitleStart").html(data[0].titleStart);
-            //$(".fst-normal").html(data[0].titleMidLine);
-            //$("#txtTitleEnd").html(data[0].titleEnd);
+            $("#txtTitleStart").append(` <em class="p-relative z-index-1 fst-normal">${data[0].titleMidLine} <span class="appear-animation" data-appear-animation="highlightScribble1"><svg class="highlight-scribble-1" viewBox="0 0 760 60" preserveAspectRatio="none"><path d="M19,49C75.19,30.63,152,21,317.26,17.27c55.43-.41,198.33-2.08,407.85,12.53" stroke="#0088cc" pathLength="1" stroke-width="14" fill="none"></path></svg></span></em>`);
+            $("#txtTitleStart").append(`<span id="txtTitleEnd">${data[0].titleEnd}</span>`);
             $("#txtContent").html(data[0].content);
 
         }
     });
-}
+
+
