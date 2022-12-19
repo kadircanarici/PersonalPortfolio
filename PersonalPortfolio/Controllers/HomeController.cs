5fd3f30bf709fe6ea9d77bccf2d8c67a0f74593a
Kadircan Arıcı <kadircanarc@gmail.com>
2022-12-19 15:08:31 +0300

    home controller intro

--- PersonalPortfolio/Controllers/HomeController.cs
+++ PersonalPortfolio/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PersonalPortfolio.Data;
 using PersonalPortfolio.Models;
 using System.Diagnostics;
 
@@ -6,13 +7,25 @@ namespace PersonalPortfolio.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ApplicationDbContext _db;
+        public HomeController(ApplicationDbContext db)
         {
-            _logger = logger;
+            _db = db;
+        }
+        public IActionResult GetIntros()
+        {
+            List<Intro> Intros = _db.Intros.ToList();
+            return Json(Intros);
         }
 
+
+
+
+
+
+
+
+        
         public IActionResult Index()
         {
             return View();
---
+++ PersonalPortfolio/wwwroot/js/Home/Intro.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    WriteIntro();
+
+}
+);
+function WriteIntro() {
+    $.ajax('Home/GetIntros', {
+        type: "GET",
+        success: function (data) {
+            $("#txtTitleStart").html(data[0].titleStart);
+            //$(".fst-normal").html(data[0].titleMidLine);
+            //$("#txtTitleEnd").html(data[0].titleEnd);
+            $("#txtContent").html(data[0].content);
+
+        }
+    });
+}
