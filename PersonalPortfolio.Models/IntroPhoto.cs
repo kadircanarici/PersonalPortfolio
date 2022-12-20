a4fbc7539c64d22c46098fbd329336db4b9b7028
Nursinem Handan Şahan <sinemsahan1995@gmail.com>
2022-12-20 14:07:58 +0300

    Sinem Intro Photo Class Eklendi

---
+++ PersonalPortfolio.Models/IntroPhoto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalPortfolio.Models
+{
+    public class IntroPhoto:ModelBase
+    {
+        public string ImagePath { get; set; }
+        public string Title { get; set; }
+    }
+}
