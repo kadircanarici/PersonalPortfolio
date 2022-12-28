b8eef6fd10ce019bf35feec488b047732114015f
Kadircan Arıcı <kadircanarc@gmail.com>
2022-12-28 15:23:49 +0300

    28-12-22

--- PersonalPortfolio/Areas/Admin/Controllers/AboutMeController.cs
+++ PersonalPortfolio/Areas/Admin/Controllers/AboutMeController.cs
@@ -21,6 +21,23 @@ namespace PersonalPortfolio.Areas.Admin.Controllers
         {
             List<AboutMe> aboutMes=_db.AboutMe.Where(a=>a.IsActive==true && a.IsDeleted == false).ToList<AboutMe>();
             return Json(new { data = aboutMes });
-        }   
+        }
+        [HttpPost]
+        public IResult Edit(AboutMe aboutMe)
+        {
+            AboutMe asil = _db.AboutMe.Find(aboutMe.Id);
+            asil.Title = aboutMe.Title;
+            asil.CurvedText = aboutMe.CurvedText;
+            asil.Description = aboutMe.Description;
+            asil.ImagePath = aboutMe.ImagePath;
+            _db.AboutMe.Update(asil);
+            _db.SaveChanges();
+            return Results.Ok("basarılı");
+        }
+        public IActionResult GetById(Guid id)
+        {
+            AboutMe aboutMe = _db.AboutMe.Find(id);
+            return Json(aboutMe);
+        }
     }
 }
