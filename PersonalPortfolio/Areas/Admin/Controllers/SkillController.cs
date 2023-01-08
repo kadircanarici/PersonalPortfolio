7a280294526650773ea7ac37f06b230120eb3e31
kadircanarici <kadircanarc@gmail.com>
2023-01-08 14:04:05 +0300

    yetenek admin ekranı tamamlandı

--- PersonalPortfolio/Areas/Admin/Controllers/SkillController.cs
+++ PersonalPortfolio/Areas/Admin/Controllers/SkillController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using PersonalPortfolio.Data;
 using PersonalPortfolio.Data.Migrations;
 using PersonalPortfolio.Models;
+using System.Data;
 using System.Linq;
 
 namespace PersonalPortfolio.Areas.Admin.Controllers
@@ -15,26 +17,47 @@ namespace PersonalPortfolio.Areas.Admin.Controllers
         {
             _db = db;
         }
+        [Authorize(Roles = "Admin")]
 
         public IActionResult Index()
         {
             return View();
         }
+        [Authorize(Roles = "Admin")]
+
         public IActionResult GetAll()
         {
             return Json(new { data = _db.skills.Where(s => s.IsActive == true && s.IsDeleted == false).ToList() }); ;
         }
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
         public IActionResult Add(Skill skill)
         {
             _db.skills.Add(skill);
             _db.SaveChanges();
             return Json(skill);
         }
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
         public IActionResult GetById(Guid id)
         {
             Skill skill = _db.skills.Find(id);
             return Json(skill);
         }
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public IResult Edit(Skill skill)
+        {
+            Skill asil = _db.skills.Find(skill.Id);
+            asil.Column = skill.Column;
+            asil.Name = skill.Name;
+            _db.skills.Update(asil);
+            _db.SaveChanges();
+
+            return Results.Ok("basarılı");
+        }
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
         public IActionResult Delete(Guid id)
         {
             Skill skll = _db.skills.Find(id);
